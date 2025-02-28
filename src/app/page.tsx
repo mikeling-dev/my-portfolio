b71c3abb20f8ea8b41a9b506ef20bb87308bd14f
Mike Ling <mikeling.dev@gmail.com>
2025-02-28 14:19:32 +0800

    moved navbar and footer component to layout.tsx to wrap the site

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import Footer from "@/components/Footer";
 export default function Home() {
   return (
     <div>
-      <Navbar />
+      {/* <Navbar /> */}
       <Hero />
       <Projects />
       <About />
